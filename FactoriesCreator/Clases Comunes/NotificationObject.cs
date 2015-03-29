f1d02c8c026212ab60dc93041d0d0e747f5c93de
Josue Cruz <ecruzj@outlook.com>
2015-03-28 19:32:46 -0600

    -Distribucion MVVM -Clase base para la estuctura del proyecto

---
+++ FactoriesCreator/Clases Comunes/NotificationObject.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace FactoriesCreator.Clases_Comunes
+{
+    public class NotificationObject : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Notifica Cambios efectuados a la propiedad
+        /// </summary>
+        /// <param name="propertyName">Propiedad que sufrio cambios</param>
+        protected void RaisePropertyChanged(string propertyName) 
+        { 
+            var handler = PropertyChanged; 
+            if (handler != null) 
+            { 
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
---
+++ FactoriesCreator/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,17 @@
+﻿
+namespace FactoriesCreator.ViewModels
+{
+    public class ViewModelLocator
+    {
+        private FactoryCreatorModel _factoryCreatorModel;
+
+        public FactoryCreatorModel FactoryCreatorModel
+        {
+            get
+            {
+                return _factoryCreatorModel ??
+                       (_factoryCreatorModel = new FactoryCreatorModel());
+            }
+        }
+    }
+}
