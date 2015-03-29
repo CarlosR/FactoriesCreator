d76360e8caece5421c1ebecef7ed623e8958e2f4
Josue Cruz <ecruzj@outlook.com>
2015-03-28 23:49:28 -0600

    Adiciones al view model del proyecto

--- FactoriesCreator/ViewModels/FactoryCreatorModel.cs
+++ FactoriesCreator/ViewModels/FactoryCreatorModel.cs
@@ -8,11 +8,51 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.ObjectModel;
 
 namespace FactoriesCreator.ViewModels
 {
-    public class FactoryCreatorModel
+    public class FactoryCreatorModel : BaseINPC
     {
+        #region Miembros privados
+        // Miembros privados 
+        #endregion
 
+        #region Constructor 
+        // Constructor
+        #endregion
+
+        #region Propiedades 
+        // Propiedades 
+
+        string QueryString
+        {
+            get { return _queryString; }
+            set
+            {
+                if (_queryString != value)
+                {
+                    _queryString = value;
+                    RaisePropertyChanged("QueryString");
+                }
+            }
+        }
+
+        private string _queryString;
+
+        #endregion
+
+        #region Metodos publicos 
+
+        // Metodos publicos 
+
+        #endregion
+
+        #region Comandos 
+
+        // Comandos 
+
+        #endregion
     }
 }
